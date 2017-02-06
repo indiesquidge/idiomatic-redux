c56f5517c7a7327ddbad401d5bf484fe61384903
Austin Wood <austin@shipt.com>
2017-02-06 15:26:36 -0800

    Extract todo reducer from todos reducer for separation of concerns

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+
+      return {
+        ...state,
+        completed: !state.completed
+      }
+  }
+}
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ]
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo
-        }
-
-        return {
-          ...todo,
-          completed: !todo.completed
-        }
-      })
+      return state.map(t => todo(t, action))
     default:
       return state
   }
