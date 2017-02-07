44eea23cd01db5ecde73b7a80830665c9a58753e
Austin Wood <austin@shipt.com>
2017-02-06 19:33:31 -0800

    Integrate toggle todo action with view

--- src/index.js
+++ src/index.js
@@ -27,7 +27,16 @@ class TodoApp extends Component {
         </button>
         <ul>
           {this.props.todos.map(todo =>
-            <li key={todo.id}>
+            <li key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                })
+              }}
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none'
+              }}>
               {todo.text}
             </li>
           )}
