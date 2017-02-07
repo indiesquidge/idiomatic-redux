3da380b56c15c2ec4d43e3d5b36dc8eeeb981fcc
Austin Wood <austin@shipt.com>
2017-02-06 19:09:14 -0800

    Implement view layer (i.e. React) for adding a todo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import todoApp from './store/Todos/reducers'
+
+import { createStore } from 'redux'
+const store = createStore(todoApp)
+
+let nextTodoId = 0
+
+class TodoApp extends Component {
+  render () {
+    return (
+      <div>
+        <input type='text' ref={node => {
+          this.input = node
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          })
+          this.input.value = ''
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root')
+  )
+}
+
+store.subscribe(render)
+render()
