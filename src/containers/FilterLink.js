70e847417a5d2ff360b7fb106cdafe69a3f3815d
Austin Wood <austin@shipt.com>
2017-02-11 01:27:40 -0600

    Simplify arrow functions with ES6 niceties

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -4,19 +4,15 @@ import Link from '../components/Link'
 
 import { setVisibilityFilter } from '../store/Todos/actions'
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    active: ownProps.filter === state.visibilityFilter
-  }
-}
+const mapStateToProps = (state, ownProps) => ({
+  active: ownProps.filter === state.visibilityFilter
+})
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    onClick: () => {
-      dispatch(setVisibilityFilter(ownProps.filter))
-    }
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick () {
+    dispatch(setVisibilityFilter(ownProps.filter))
   }
-}
+})
 
 const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link)
 
