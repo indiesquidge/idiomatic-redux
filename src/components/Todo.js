16850f43e0e905c557b242622af6519fec36a1ea
Austin Wood <austin@shipt.com>
2017-02-10 19:54:17 -0600

    Extract presentational components Todo and TodoList

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todo = ({ onClick, completed, text }) => (
+  <li onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}>
+    {text}
+  </li>
+)
+
+export default Todo
--- src/index.js
+++ src/index.js
@@ -2,31 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 import todoApp from './store/Todos/reducers'
+import { createStore } from 'redux'
 
-const createStore = (rootReducer) => {
-  let state
-  let listeners = []
-
-  const getState = () => state
-
-  const dispatch = (action) => {
-    state = rootReducer(state, action)
-    listeners.forEach(listener => listener())
-  }
-
-  const subscribe = (listener) => {
-    listeners.push(listener)
-    // unsubscribe return function
-    return () => {
-      listeners.filter(l => l !== listener)
-    }
-  }
-
-  // dummy action to get reducer to return initial state
-  dispatch({})
-
-  return { getState, dispatch, subscribe }
-}
+import TodoList from './components/TodoList'
 
 const store = createStore(todoApp)
 
@@ -85,22 +63,15 @@ class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                })
-              }}
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none'
-              }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          }
+        />
         <p>
           Show:
           {' '}
