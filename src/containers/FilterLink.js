f4a6781a76b0efda4ec0f5ae051abba44f588ba0
Austin Wood <austin@shipt.com>
2017-02-11 00:57:04 -0600

    Generate AddTodo container with passed in props

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,40 +1,24 @@
-import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Link from '../components/Link'
 
-export default class FilterLink extends Component {
-  componentDidMount () {
-    const { store } = this.context
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    )
-  }
-
-  componentWillUnmount () {
-    this.unsubscribe()
-  }
-
-  render () {
-    const props = this.props
-    const { store } = this.context
-    const state = store.getState()
-
-    return (
-      <Link
-        active={props.filter === state.visibilityFilter}
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }
-      >
-        {props.children}
-      </Link>
-    )
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
   }
 }
 
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      })
+    }
+  }
 }
+
+const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link)
+
+export default FilterLink
