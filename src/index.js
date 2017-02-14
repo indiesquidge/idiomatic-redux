43c4fe077a0fa227e9f4d4f08231a347efe9b754
Austin Wood <austin@shipt.com>
2017-02-14 07:08:43 -0800

    Throttle our expensive save to localStorage to max 1 sec

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import throttle from 'lodash/throttle'
 
 import TodoApp from './components/TodoApp'
 import todoApp from './store/Todos/reducers'
@@ -11,11 +12,11 @@ const persistedState = loadState()
 
 const store = createStore(todoApp, persistedState)
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   })
-})
+}, 1000))
 
 ReactDOM.render(
   <Provider store={store}>
