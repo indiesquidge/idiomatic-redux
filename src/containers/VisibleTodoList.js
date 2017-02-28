e7f9c863b7a1d3a614e43456f8f8c690a74c34cf
Austin Wood <i@austinwood.me>
2017-02-28 13:20:23 -0800

    Fetch todo data on component mount and route change

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,14 +1,43 @@
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import TodoList from '../components/TodoList'
 import { getVisibleTodos } from '../store/Todos/reducers'
 import { toggleTodo } from '../store/Todos/actions'
+import { fetchTodos } from '../api'
 
-const mapStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state, match.params.filter || 'all')
-})
+class VisibleTodoList extends Component {
+  componentDidMount () {
+    fetchTodos(this.props.filter).then(todos => {
+      console.log(todos)
+    })
+  }
 
-const VisibleTodoList = withRouter(connect(mapStateToProps, { onTodoClick: toggleTodo })(TodoList))
+  componentDidUpdate (prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos => {
+        console.log(todos)
+      })
+    }
+  }
+
+  render () {
+    return <TodoList {...this.props} />
+  }
+}
+
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all'
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  }
+}
+
+VisibleTodoList = withRouter(connect(
+  mapStateToProps,
+  { onTodoClick: toggleTodo }
+)(VisibleTodoList))
 
 export default VisibleTodoList
