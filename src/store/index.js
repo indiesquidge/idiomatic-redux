ddc9dbe3e93be742f7e162e717257313dc816688
Austin Wood <i@austinwood.me>
2017-02-28 15:47:22 -0800

    Generalize wrapping `dispatch()` into a concept called "middleware"

--- src/store/index.js
+++ src/store/index.js
@@ -2,42 +2,44 @@ import { createStore } from 'redux'
 
 import todos from './Todos/reducers'
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch
+const logging = (store) => (next) => {
   if (!console.group) {
-    return rawDispatch
+    return next
   }
 
   return (action) => {
     console.group(action.type)
     console.log('%c prev state', 'color: grey', store.getState())
     console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
+    const returnValue = next(action)
     console.log('%c next state', 'color: green', store.getState())
     console.groupEnd(action.type)
     return returnValue
   }
 }
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch)
-    }
-    return rawDispatch(action)
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next)
   }
+  return next(action)
+}
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch)
+  })
 }
 
 const configureStore = () => {
   const store = createStore(todos)
+  let middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logging)
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store)
+  wrapDispatchWithMiddlewares(store, middlewares)
 
   return store
 }
