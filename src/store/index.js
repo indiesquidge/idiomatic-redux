0bfe2fbf9f8a139af015bcedb5a955744d836480
Austin Wood <i@austinwood.me>
2017-02-28 11:51:46 -0800

    Log state change along with the action that caused it

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,31 @@ import throttle from 'lodash/throttle'
 import todos from './Todos/reducers'
 import { loadState, saveState } from './localStorage'
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+  if (!console.group) {
+    return rawDispatch
+  }
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: grey', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
 const configureStore = () => {
   const persistedState = loadState()
 
   const store = createStore(todos, persistedState)
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
 
   store.subscribe(throttle(() => {
     saveState({
