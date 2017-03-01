e5f3856061b60c5be46cb7c3bd21feec908b4df4
Austin Wood <i@austinwood.me>
2017-02-28 16:20:29 -0800

    Replace hand-rolled middleware with core and third-party utilities

--- src/store/index.js
+++ src/store/index.js
@@ -1,47 +1,20 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 
 import todos from './Todos/reducers'
 
-const logging = (store) => (next) => {
-  if (!console.group) {
-    return next
-  }
-
-  return (action) => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: grey', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(next)
-  }
-  return next(action)
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch)
-  })
-}
-
 const configureStore = () => {
-  const store = createStore(todos)
   let middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logging)
+    middlewares.push(createLogger())
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares)
-
-  return store
+  return createStore(
+    todos,
+    applyMiddleware(...middlewares)
+  )
 }
 
 export default configureStore
