72fd70546061cba2f8c51e3093ce62b19a53b7c3
Austin Wood <i@austinwood.me>
2017-03-01 11:45:10 -0800

    Use redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 
 import todos from './Todos/reducers'
-
-const thunk = (store) => (next) => (action) =>
-  (typeof action === 'function')
-    ? action(store.dispatch, store.getState)
-    : next(action)
 
 const configureStore = () => {
   let middlewares = [thunk]
